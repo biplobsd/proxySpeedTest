8ebb466b98c16f2bc11a1b42241c761fc030cd36
biplobsd <biplobsd11@gmail.com>
2019-11-03 12:11:53 +0600

    added protocol selection asking

--- proxySpeedTest.py
+++ proxySpeedTest.py
@@ -4,6 +4,8 @@ from collections import OrderedDict
 from urllib import error, request
 import urllib.request
 from tqdm import tqdm
+import socks
+import sockshandler
 
 def buildRange(value, numsplits):
     lst = []
@@ -18,6 +20,7 @@ def buildRange(value, numsplits):
 def speedTest(ip):
 	#mirror = "https://drive.google.com/uc?id=0Bzkrq-7orwGScTAxNkFDaTM0Rkk&authuser=0&export=download"
 	mirror = 'http://speedtest.tele2.net/1MB.zip'
+	global protocol
 	socket.setdefaulttimeout(30)
 	filename = '1MB.zip'
 	start = time.time()
@@ -27,13 +30,27 @@ def speedTest(ip):
 	proxy_ip = ip.strip()
 	def downloadChunk(idx, irange):
 		try:
-			proxy_handler = urllib.request.ProxyHandler({'http': proxy_ip})
+			if protocol in ('http'):
+				proxy_handler = urllib.request.ProxyHandler({'http': proxy_ip,})
+			if protocol in ('https'):
+				proxy_handler = urllib.request.ProxyHandler({'https': proxy_ip,})
+			elif protocol is 'sock4':
+				ip,port = proxy_ip.split(':')
+				proxy_handler = sockshandler.SocksiPyHandler(socks.SOCKS4, ip, int(port))
+			elif protocol is 'sock5':
+				ip,port = proxy_ip.split(':')
+				proxy_handler = sockshandler.SocksiPyHandler(socks.SOCKS5, ip, int(port))
+
 			opener = urllib.request.build_opener(proxy_handler)
 			urllib.request.install_opener(opener)
 			req = urllib.request.Request(mirror)
 			req.add_header('Range', f'bytes={irange}')
+
 			dataDict[idx] = urllib.request.urlopen(req).read()
 
+			# print("urlopen :")
+			# print(type(dataDict[idx]))
+			# print("")
 		except error.URLError:
 			 return print(f"\nInvalid ip or timeout for {proxy_ip}")
 		except ConnectionResetError:
@@ -74,9 +91,31 @@ def speedTest(ip):
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def whichProtocol(question, default="http"):
+	valid = {"1": 'http', "2": 'https', "3": 'sock4',
+ 		"4": 'sock5'}
+	if default is None:
+		prompt = "\n\n1. http\n2. https\n3. sock4\n4. sock5 "
+	elif default is 'http':
+		prompt = f" \n\n1. http\n2. https\n3. sock4\n4. sock5"
+	else:
+		raise ValueError("\n\ninvalid default answer: '%s'" % default)
+
+	while True:
+		sys.stdout.write(prompt + question+f'[default={default}]: ')
+		choice = input().lower()
+		if default is not None and choice == '':
+			return valid[default]
+		elif choice in valid:
+			return valid[choice]
+		else:
+			clear()
+			sys.stdout.write("\n\nError : Please respond with Number[1/2/3/4] \n")
+
 unsort = []
 sort = []
 proxyslist = []
+
 open('proxys.txt', 'a+').close()
 handle = open('proxys.txt')
 for line in handle:
@@ -84,28 +123,27 @@ for line in handle:
 		proxyslist.append(line)
 handle.close()
 
-try:
-	if not len(proxyslist) == 0:
-		for i in tqdm(proxyslist):
-			p = speedTest(i)
-			clear()
-			print(p)
-			if not (p[0] == 'C' or p[0] == 'I' or p[0] == 'Y'):
-				ip , kb = p.split('d:')
-				kb1 , kb = kb.split(' KB/s')
-				unsort.append({'ip':ip+'d','speed':int(kb1)})
-				sort = sorted(
-				    unsort,
-				    key=lambda x: x['speed'], reverse=True)
-			print("\nSort as Speed: (Top 10)")
-			count = 0
-			for p in sort:
-				count += 1
-				print(p['ip']+'\t'+str(p['speed'])+' KB/s')
-				if count == 10:
-					break
-			print("\nChecking ...")
-	else:
-		print("Import some proxys(IP:prot) in proxys.txt file.")
-except KeyboardInterrupt:
-	print("\n\nExited by User.")
+if not len(proxyslist) == 0:
+	protocol = whichProtocol("\n\nWhich's protocol do you want use with ")
+	clear()
+	for i in tqdm(proxyslist):
+		p = speedTest(i)
+		clear()
+		print(p)
+		if not (p[0] == 'C' or p[0] == 'I' or p[0] == 'Y'):
+			ip , kb = p.split('d:')
+			kb1 , kb = kb.split(' KB/s')
+			unsort.append({'ip':ip+'d','speed':int(kb1)})
+			sort = sorted(
+			    unsort,
+			    key=lambda x: x['speed'], reverse=True)
+		print("\nSort as Speed: (Top 10)")
+		count = 0
+		for p in sort:
+			count += 1
+			print(p['ip']+'\t'+str(p['speed'])+' KB/s')
+			if count == 10:
+				break
+		print("\nChecking ...")
+else:
+	print("Import some proxys(IP:prot) in proxys.txt file.")
